cc272cc57a3af31c87cc2f45107c90bd5c4d0358
Niklas Schmitt <niklas.schmitt@mailbox.org>
2022-05-16 19:33:42 +0200

    feat(Pollenflug): initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pollenflug!
+this is a website shows the current Pollenbelastung in the air. data provided by DWD https://www.dwd.de/DE/leistungen/gefahrenindizespollen/gefahrenindexpollen.html#buehneTop
+images provided by https://github.com/leahoswald/lovelace-dwd-pollenprognose-card
---
+++ lib/DANCER/Server.pm
@@ -0,0 +1,91 @@
+#!/usr/bin/perl
+
+# Copyright Key-Systems GmbH
+package DANCER::Server;
+use strict;
+use warnings;
+use Dancer2 appname => 'DANCER::Server';
+use Data::Dumper;
+use LWP::UserAgent;
+use JSON;
+use Cwd qw(cwd getcwd);
+
+set charset => 'utf8';
+
+# Note that the public directory name is not included in the URL.
+# A file ./public/css/style.css is made available as http://example.com/css/style.css
+
+get '/' => sub {
+	redirect "/index";
+};
+
+get '/index' => sub {
+	my $lastUpdate = "n/a";
+	my $nextUpdate = "n/a";
+
+	my $legend;
+	$legend->{"0"} = "keine Belastung";
+	$legend->{"0-1"} = "keine bis geringe Belastung";
+	$legend->{"1"} = "geringe Belastung";
+	$legend->{"1-2"} = "geringe bis mittlere Belastung";
+	$legend->{"2"} = "mittlere Belastung";
+	$legend->{"2-3"} = "mittlere bis hohe Belastung";
+	$legend->{"3"} = "keine Belastung";
+
+	my @tableData;
+
+	my $dwd = "https://opendata.dwd.de/climate_environment/health/alerts/s31fg.json";
+
+	my $json = JSON->new()->canonical(1);
+	$json->utf8(1);
+	my $ua = new LWP::UserAgent;
+	my $request = new HTTP::Request('GET', $dwd);
+	my $response = $ua->request($request);
+	my $data;
+
+	if ($response->is_success)
+	{
+		$data = $json->decode($response->content);
+		my $pollen = $data->{'content'};
+
+		$nextUpdate = $data->{'next_update'};
+		$lastUpdate = $data->{'last_update'};
+
+		my $i = 0;
+		while (defined($data->{'content'}[$i]))
+		{
+			my $info = $data->{'content'}[$i];
+			my $weed;
+
+			foreach my $types (qw(Birke Erle Hasel Esche Graeser Roggen Beifuss Ambrosia))
+			{
+				$weed->{lc($types)} = $info->{'Pollen'}{$types}{'today'};
+				$weed->{lc($types).'legend'} = "$types - $info->{'Pollen'}{$types}{'today'} - $legend->{$info->{'Pollen'}{$types}{'today'}}";
+			}
+
+			$weed->{'region'} = $info->{'region_name'};
+			$weed->{'partregion'} = $info->{'partregion_name'};
+			if ($weed->{'partregion'} eq "")
+			{
+				$weed->{'partregion'} = $weed->{'region'};
+				$weed->{'region'} = "";
+			}
+
+			push @tableData, $weed;
+			$i++;
+		}
+	}
+	else
+	{
+		template "index", {error => "error while requesting data from DWD: $response->status_line\n", lastUpdate => "n/a", nextUpdate => "n/a"};
+	}
+
+
+	template "index", {lastUpdate => $lastUpdate, nextUpdate => $nextUpdate, tableData => \@tableData};
+};
+
+# get '/refresh' => sub {
+# 	#todo new json request
+# };
+
+dance;
